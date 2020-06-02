88c28adfd8033799f5eb62c02fc2a78d613cbadc
wanggy5 <wanggy5@asiainfo.com>
2020-06-03 07:48:06 +0800

    [新增]学习spring

--- spring-05-autowired/src/main/java/com/wang/pojo/People.java
+++ spring-05-autowired/src/main/java/com/wang/pojo/People.java
@@ -1,18 +1,30 @@
 package com.wang.pojo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.lang.Nullable;
+
+import javax.annotation.Resource;
+
 /**
  * @author wanggy5
  * @date 2020/5/24 23:57
  */
 public class People {
+    //如果显式的定义了Autowired的required属性为false，说明这个对象可以为null，否则不允许。
+//    @Autowired
+//    @Qualifier(value = "cat222")
+    @Resource(name = "cat")
     private Cat cat;
+    @Autowired
     private Dog dog;
+
     private String name;
 
     public Cat getCat() {
         return cat;
     }
-
+    @Autowired
     public void setCat(Cat cat) {
         this.cat = cat;
     }
@@ -21,6 +33,7 @@ public class People {
         return dog;
     }
 
+    @Autowired
     public void setDog(Dog dog) {
         this.dog = dog;
     }
